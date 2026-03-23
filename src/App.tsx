698fbb838a1cc48578b7ca391c632d934dfed2c7
Ievgen Prygoda <e.klimochenkova@gmail.com>
2026-03-23 20:42:32 +0000

    route import fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import { RouterProvider } from "react-router-dom";
 
 import { theme } from "shared/theme/theme";
-import { router } from "router";
+import { router } from "./router";
 
 function App() {
   return (
