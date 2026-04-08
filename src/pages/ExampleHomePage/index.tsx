5b36dd521851d2fa0f6a9ea51d2aa3d1664d33de
Oleksandr Polishchuk <aleksice1p@gmail.com>
2026-04-08 10:14:49 +0300

    Fixing  translation.json  end layout for resolving  errors

--- src/pages/ExampleHomePage/index.tsx
+++ src/pages/ExampleHomePage/index.tsx
@@ -1,27 +1,13 @@
-
-import { Container, Typography } from "@mui/material";
-import { useTranslation } from "react-i18next";
-
-import Footer from "components/Footer";
-import { ExampleForm } from "features/ExampleForm";
 import { ComplianceSection } from "features/ComplianceSection";
 import { Loader } from "shared/ui/Loader";
 import { Capabilities } from "features/Capabilities";
 
-import { HomePageTitle } from "./styles";
-
 export default function ExampleHomePage() {
   return (
     <>
-      <Container>
-        <HomePageTitle variant="h1">{t("exampleHomePage.title")}</HomePageTitle>
-        <Typography variant="body1">{t("exampleHomePage.text")}</Typography>
-        <ExampleForm />
-         <Capabilities />
-        <ComplianceSection />
-        <Loader />
-      </Container>
-      <Footer />
+      <Capabilities />
+      <ComplianceSection />
+      <Loader />
     </>
   );
 }
