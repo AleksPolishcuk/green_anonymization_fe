3680fe2580fe6aa6434dbbac2284f0d3b9a209e1
Ievgen Prygoda <e.klimochenkova@gmail.com>
2026-04-09 22:26:01 +0100

    scroll to top component added, anchor added

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -2,15 +2,20 @@ import type { ReactNode } from "react";
 
 import Header from "components/Header";
 import Footer from "components/Footer";
+import { ScrollToTop } from "components/ScrollToTop";
 
 type LayoutProps = {
   children: ReactNode;
   headerOverlay?: boolean;
 };
 
-export const MainLayout = ({ children, headerOverlay = false }: LayoutProps) => {
+export const MainLayout = ({
+  children,
+  headerOverlay = false,
+}: LayoutProps) => {
   return (
     <>
+      <ScrollToTop />
       <Header overlay={headerOverlay} />
       <main>{children}</main>
       <Footer />
