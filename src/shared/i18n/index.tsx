627059a9c18eded54957468725f1213f01ec7a2f
riznyk.serhii.v@gmail.com <riznyk.serhii.v@gmail.com>
2026-04-14 21:12:42 +0200

    fix: rename notFound.json to notfound.json (case-sensitive)

--- src/shared/i18n/index.tsx
+++ src/shared/i18n/index.tsx
@@ -5,7 +5,7 @@ import enTranslation from "./locales/en/translation.json";
 import enHero from "./locales/en/hero.json";
 import enCapabilities from "./locales/en/capabilities.json";
 import enReadyToProtect from "./locales/en/readyToProtect.json";
-import enNotFound from "./locales/en/notFound.json";
+import enNotFound from "./locales/en/notfound.json";
 
 void i18n.use(initReactI18next).init({
   resources: {
