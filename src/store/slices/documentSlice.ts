6242142afc813c0e7e907a12e5fd89806add3ad9
riznyk.serhii.v@gmail.com <riznyk.serhii.v@gmail.com>
2026-05-06 13:31:04 +0200

    fix: initial Document state step

--- src/store/slices/documentSlice.ts
+++ src/store/slices/documentSlice.ts
@@ -9,7 +9,7 @@ import { DOCUMENT_MOCK } from "store/mocks/documentMock";
 import { DEID_STEPS } from "constants/MainPages";
 
 const initialState: DocumentState = {
-  currentStep: "results",
+  currentStep: "framework",
   entities: DOCUMENT_MOCK.entities,
   originalText: DOCUMENT_MOCK.originalText,
   redactedText: DOCUMENT_MOCK.redactedText,
