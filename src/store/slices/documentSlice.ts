6376ca8c2d4a73f49254b9ba721b33e727faeb53
Danil2537 <dedov7347@gmail.com>
2026-05-09 16:19:03 +0300

    why this change wasn't added to merge? idk

--- src/store/slices/documentSlice.ts
+++ src/store/slices/documentSlice.ts
@@ -8,17 +8,12 @@ import { DEID_STEPS } from "constants/MainPages";
 import { complianceService } from "services/compliance";
 import type { PiiEntity } from "services/input/typing";
 import type {
-  ComplianceFramework,
   DeidStep,
   Document,
   DocumentState,
   Entity,
 } from "store/types/document";
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { ComplianceFramework } from "services/compliance/typing/compliance";
-import type { DeidStep, DocumentState, Entity } from "store/types/document";
-import { DEID_STEPS } from "constants/MainPages";
-import type { PiiEntity } from "services/input/typing";
 
 const initialState: DocumentState = {
   currentStep: "framework",
@@ -135,10 +130,12 @@ export const saveFrameworkSelection = createAsyncThunk<
   { rejectValue: string }
 >(
   "document/saveFramework",
-  async (frameworkCode, { dispatch, rejectWithValue }) => {
-    dispatch(setSelectedFramework(frameworkCode));
+  async (framework: ComplianceFramework, { dispatch, rejectWithValue }) => {
+    dispatch(setSelectedFramework(framework));
     try {
-      await complianceService.selectFramework({ frameworkCode });
+      await complianceService.selectFramework({
+        frameworkCode: framework.code,
+      });
     } catch (err) {
       return rejectWithValue(
         err instanceof Error
