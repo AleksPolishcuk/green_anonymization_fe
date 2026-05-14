4b246d1947e17422ad9ad5b950353c031a9a8304
OlehShpunt <olehshpunt2@gmail.com>
2026-05-14 16:02:25 +0300

    fix: adapt synthetic data table margins to new page layout

--- src/components/SyntheticDataContents/index.tsx
+++ src/components/SyntheticDataContents/index.tsx
@@ -1,15 +1,8 @@
 import { useAppSelector } from "store/hooks";
-import { SyntheticDataRoot } from "./styles";
 import SyntheticDataGeneratedDataset from "./syntheticDataGeneratedDataset";
 
 export default function SyntheticDataContents() {
   const { syntheticDocuments } = useAppSelector((state) => state.syntheticData);
 
-  return (
-    <>
-      <SyntheticDataRoot>
-        {syntheticDocuments && <SyntheticDataGeneratedDataset />}
-      </SyntheticDataRoot>
-    </>
-  );
+  return <>{syntheticDocuments && <SyntheticDataGeneratedDataset />}</>;
 }
