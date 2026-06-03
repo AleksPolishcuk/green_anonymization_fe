d811b6c07ce3fbbc40ae6af4f166e2eff953a754
Ievgen Prygoda <e.klimochenkova@gmail.com>
2026-06-03 17:56:09 +0100

    error page added

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
 import { MainLayout, WorkspaceLayout } from "Layouts";
 
@@ -15,107 +15,114 @@ import {
   Register,
   SignIn,
   SyntheticDataPage,
+  AppErrorPage,
 } from "./lazyPages";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: (
-      <MainLayout headerOverlay>
-        <HomePage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/compliance",
-    element: (
-      <MainLayout>
-        <ComplianceSection />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/pricing",
-    element: (
-      <MainLayout>
-        <PricingPage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/contactus",
-    element: (
-      <MainLayout>
-        <ContactUsPage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "sign-in",
-    element: <SignIn />,
-  },
-  {
-    path: "register",
-    element: <Register />,
-  },
-  {
-    path: "auth-callback",
-    element: <AuthCallback />,
-  },
-  {
-    path: "dashboard",
-    element: (
-      <WorkspaceLayout>
-        <Dashboard />
-      </WorkspaceLayout>
-    ),
-  },
-  {
-    path: "deidentification",
-    element: (
-      <WorkspaceLayout>
-        <DeidentificationPage />
-      </WorkspaceLayout>
-    ),
-  },
-  {
-    path: "syntheticdata",
-    element: (
-      <WorkspaceLayout>
-        <SyntheticDataPage />
-      </WorkspaceLayout>
-    ),
-  },
-  {
-    path: "profile",
-    element: (
-      <WorkspaceLayout>
-        <ProfilePage />
-      </WorkspaceLayout>
-    ),
-  },
-  {
-    path: "/terms",
-    element: (
-      <MainLayout>
-        <NotFound />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/privacy",
-    element: (
-      <MainLayout>
-        <NotFound />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "*",
-    element: (
-      <MainLayout>
-        <NotFound />
-      </MainLayout>
-    ),
+    element: <Outlet />,
+    errorElement: <AppErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <MainLayout headerOverlay>
+            <HomePage />
+          </MainLayout>
+        ),
+      },
+      {
+        path: "/compliance",
+        element: (
+          <MainLayout>
+            <ComplianceSection />
+          </MainLayout>
+        ),
+      },
+      {
+        path: "/pricing",
+        element: (
+          <MainLayout>
+            <PricingPage />
+          </MainLayout>
+        ),
+      },
+      {
+        path: "/contactus",
+        element: (
+          <MainLayout>
+            <ContactUsPage />
+          </MainLayout>
+        ),
+      },
+      {
+        path: "sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "auth-callback",
+        element: <AuthCallback />,
+      },
+      {
+        path: "dashboard",
+        element: (
+          <WorkspaceLayout>
+            <Dashboard />
+          </WorkspaceLayout>
+        ),
+      },
+      {
+        path: "deidentification",
+        element: (
+          <WorkspaceLayout>
+            <DeidentificationPage />
+          </WorkspaceLayout>
+        ),
+      },
+      {
+        path: "syntheticdata",
+        element: (
+          <WorkspaceLayout>
+            <SyntheticDataPage />
+          </WorkspaceLayout>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <WorkspaceLayout>
+            <ProfilePage />
+          </WorkspaceLayout>
+        ),
+      },
+      {
+        path: "/terms",
+        element: (
+          <MainLayout>
+            <NotFound />
+          </MainLayout>
+        ),
+      },
+      {
+        path: "/privacy",
+        element: (
+          <MainLayout>
+            <NotFound />
+          </MainLayout>
+        ),
+      },
+      {
+        path: "*",
+        element: (
+          <MainLayout>
+            <NotFound />
+          </MainLayout>
+        ),
+      },
+    ],
   },
 ]);
